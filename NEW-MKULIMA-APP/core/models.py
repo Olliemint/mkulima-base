63ef31c44c5900c437386de5c01293dde5f54d69
Misigomartin <Misigomartin@gmail.com>
2022-07-13 17:50:58 +0300

    added models to database

--- NEW-MKULIMA-APP/core/models.py
+++ NEW-MKULIMA-APP/core/models.py
@@ -21,3 +21,53 @@ class UserToken(models.Model):
 class Reset(models.Model):
     email = models.CharField(max_length=255)
     token = models.CharField(max_length=255, unique=True)  
+
+class Feeds(models.Model):
+    title = models.CharField(max_length=200, blank=True)
+    image = models.ImageField(upload_to='media/posts', default="default.jpg")
+    description = models.TextField(max_length=300, blank=True)
+    comments = models.ManyToManyField('Comment', related_name='farmer_post',blank=True)
+    author =models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    comment = models.TextField(max_length=200, blank=True)
+    author =models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    feeds=models.ForeignKey(Feeds, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.comment
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    phone_number = models.CharField(max_length=100, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    image = models.ImageField(upload_to='media/profiles', default="default.jpg")
+
+    def __str__(self):
+        return self.user.email
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, blank=True)
+    def __str__(self):
+        return self.name
+
+
+
+class Merchandise(models.Model):
+    name = models.CharField(max_length=200, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='media/merchandise', default="default.jpg")
+    date_posted = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    
+
+
+    def __str__(self):
+        return self.name
