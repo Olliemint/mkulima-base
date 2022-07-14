7c3662a383ef384ee1818ac1149b2b8bee69f203
Misigomartin <Misigomartin@gmail.com>
2022-07-14 22:33:37 +0300

    added data pagination

--- NEW-MKULIMA-FRONTEND/mkulima-auth/src/app/app.module.ts
+++ NEW-MKULIMA-FRONTEND/mkulima-auth/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +39,8 @@ import { ContactComponent } from './contact/contact.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxPaginationModule,
   ],
   providers: [
     {
